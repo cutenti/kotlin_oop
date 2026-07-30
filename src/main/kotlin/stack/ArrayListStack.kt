bf11d95402dc59dc912237952b7e6b39f53ee866
Дарья Торгова <dtorgova@gmail.com>
2026-07-30 19:28:17 +0300

    hierarchy of collections

--- src/main/kotlin/stack/ArrayListStack.kt
+++ src/main/kotlin/stack/ArrayListStack.kt
@@ -1,5 +1,26 @@
 package org.example.stack
 
-class ArrayListStack : Stack {
-    // TODO: use delegation instead of inheritance here, use CustomArrayList
+import org.example.list.CustomArrayList
+import org.example.list.CustomList
+
+class ArrayListStack(
+    private val innerList: CustomArrayList = CustomArrayList(10)
+) : Stack, CustomList by innerList {
+
+    override fun push(value: Int) {
+        innerList.add(value)
+    }
+
+    override fun pop(): Int {
+        if (isEmpty) throw NoSuchElementException("Stack is empty")
+        return innerList.removeAt(innerList.size - 1)
+    }
+
+    override fun peek(): Int {
+        if (isEmpty) throw NoSuchElementException("Stack is empty")
+        return innerList.get(innerList.size - 1)
+    }
+
+    override val isEmpty: Boolean
+        get() = innerList.size == 0
 }
